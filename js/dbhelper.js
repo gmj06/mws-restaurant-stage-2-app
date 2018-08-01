f79a4fb0351fb89152186fc61851fa87692ae958
Gowri Jagadeesh <gowrimj2006@gmail.com>
2018-07-31 19:31:21 -0700

    Added idb.js library for IndexDBPromised, implemented methods to read from api and insert into indexdb, created manifest.json

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -1,3 +1,6 @@
+let restaurantNeighborhoods;
+let restaurantCuisines;
+
 /**
  * Common database helper functions.
  */
@@ -8,27 +11,60 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    const port = 1337 // Change this to your server port
+    return `http://localhost:${port}/restaurants`;
   }
 
   /**
    * Fetch all restaurants.
    */
-  static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
+  static fetchRestaurants(callback, id) {
+    console.log("dbhelper .. objectstore ", IDBHelper.name);
+    return IDBHelper.fetchFromIDB().then(restaurants => {
+      console.log("idbhelper .. fetchRestaurants", restaurants);
+      if (restaurants.length > 0) {
+        return Promise.resolve(restaurants);
+      } else {
+        return IDBHelper.fetchFromAPIInsertIntoIDB(id);
       }
-    };
-    xhr.send();
+    }).then(restaurants => {
+      console.log("dbhelper .. restaurants...", restaurants);
+      const neighborhoods = restaurants.map((v, i) => restaurants[i].neighborhood);
+      restaurantNeighborhoods = neighborhoods.filter((v, i) => neighborhoods.indexOf(v) == i);
+
+      const cuisines = restaurants.map((v, i) => restaurants[i].cuisine_type);
+      restaurantCuisines = cuisines.filter((v, i) => cuisines.indexOf(v) == i);
+      callback(null, restaurants);
+    }).catch(error => {
+      callback(error, null);
+    });
+
+
+    // fetch(DBHelper.DATABASE_URL)
+    //   .then(response => response.json())
+    //   .then(restaurants => {
+    //     if(restaurants){
+    //       callback(null, restaurants);
+    //     }
+    //   }).catch(error => {
+    //     callback(`Request failed. Returned status of ${error}`, null);
+    //   });
+
+
+    // let xhr = new XMLHttpRequest();
+    // xhr.open('GET', DBHelper.DATABASE_URL);
+    // xhr.onload = () => {
+    //   if (xhr.status === 200) { // Got a success response from server!
+    //     const json = JSON.parse(xhr.responseText);
+    //     const restaurants = json.restaurants;
+    //     console.log("fetchRestaurants...", restaurants);
+    //     callback(null, restaurants);
+    //   } else { // Oops!. Got an error from server.
+    //     const error = (`Request failed. Returned status of ${xhr.status}`);
+    //     callback(error, null);
+    //   }
+    // };
+    // xhr.send();
   }
 
   /**
@@ -40,6 +76,7 @@ class DBHelper {
       if (error) {
         callback(error, null);
       } else {
+        console.log("dbhelper .. fetchRestaurantById ...", restaurants);
         const restaurant = restaurants.find(r => r.id == id);
         if (restaurant) { // Got the restaurant
           callback(null, restaurant);
@@ -54,6 +91,7 @@ class DBHelper {
    * Fetch restaurants by a cuisine type with proper error handling.
    */
   static fetchRestaurantByCuisine(cuisine, callback) {
+
     // Fetch all restaurants  with proper error handling
     DBHelper.fetchRestaurants((error, restaurants) => {
       if (error) {
@@ -107,12 +145,17 @@ class DBHelper {
    * Fetch all neighborhoods with proper error handling.
    */
   static fetchNeighborhoods(callback) {
+    if(restaurantNeighborhoods){
+      callback(null, restaurantNeighborhoods);
+      return;
+    }
     // Fetch all restaurants
     DBHelper.fetchRestaurants((error, restaurants) => {
       if (error) {
         callback(error, null);
       } else {
         // Get all neighborhoods from all restaurants
+        console.log("fetchNeighborhoods ", restaurants);
         const neighborhoods = restaurants.map((v, i) => restaurants[i].neighborhood)
         // Remove duplicates from neighborhoods
         const uniqueNeighborhoods = neighborhoods.filter((v, i) => neighborhoods.indexOf(v) == i)
@@ -125,6 +168,11 @@ class DBHelper {
    * Fetch all cuisines with proper error handling.
    */
   static fetchCuisines(callback) {
+    if(restaurantCuisines){
+      callback(null, restaurantCuisines);
+      return;
+    }
+
     // Fetch all restaurants
     DBHelper.fetchRestaurants((error, restaurants) => {
       if (error) {
@@ -150,7 +198,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant, type) {
-    return (`/img/${type}/${restaurant.photograph}`);
+    return (`/img/${type}/${restaurant.id}.jpg`);
   }
 
   /**
@@ -162,7 +210,8 @@ class DBHelper {
       title: restaurant.name,
       url: DBHelper.urlForRestaurant(restaurant),
       map: map,
-      animation: google.maps.Animation.DROP}
+      animation: google.maps.Animation.DROP
+    }
     );
     return marker;
   }
--- service_worker.js
+++ service_worker.js
@@ -7,13 +7,14 @@
         "/",
         "/index.html",
         "/restaurant.html",
-        "/css/styles.css",
-        "/data/restaurants.json",
-        "/js/",
+        "/css/styles.css",       
+        "/js/idb.js",
+        "/js/idbhelper.js",
         "/js/dbhelper.js",
         "/js/main.js",
         "/js/restaurant_info.js",
-        "/js/register_service_worker.js"
+        "/js/register_service_worker.js",
+        "/manifest.json"
     ];
 
     self.addEventListener("install", event => {
@@ -44,21 +45,61 @@
         );
     });
 
+    
     self.addEventListener("fetch", event => {
-        const cacheRequest = event.request;        
-        event.respondWith(
-            caches.match(cacheRequest).then(resp => {
-                return resp || fetch(event.request).then(response => {
-                    let responseClone = response.clone();
+        let type = '';
+        let requestUrl = new URL(event.request.url);
 
-                    caches.open(staticCacheName).then(cache => {
-                        cache.put(event.request, responseClone)
-                    })
-                    return response;
+        if (requestUrl.pathname === '/' || requestUrl.pathname === '/index.html') {
+            type = '/index.html';
+        } else if (requestUrl.pathname.indexOf('/restaurant.html') > -1) {
+            type = '/restaurant.html';
+        }
+
+        if (type != '') {
+            event.respondWith(
+                caches.open(staticCacheName).then(cache => {
+                    return cache.match(type).then(response => {
+                        console.log("type...", type);
+                        console.log("response...", response);
+                        let fetchPromise = fetch(type)
+                            .then(ntwkResponse => {
+                                cache.put(type, ntwkResponse.clone());
+                                return ntwkResponse;
+                            });
+                        return response || ntwkResponse;
+                    });
                 })
-            }).catch(err => {
-                console.log("err in fetch for " + event.request.url, err);
-            })
-        )
-    });    
+            );
+        }
+        else if (urlsToCache.includes(requestUrl.href) || urlsToCache.includes(requestUrl.pathname)) {
+            event.respondWith(
+                caches.open(staticCacheName).then(cache => {
+                    return cache.match(event.request).then(response => {
+                        return response || fetch(event.request);
+                    });
+                }).catch(err => {
+                    console.log("err in fetch for " + event.request.url, err);
+                })
+            )
+        }
+    });
+
+    // self.addEventListener("fetch", event => {
+    //     const cacheRequest = event.request;        
+    //     event.respondWith(
+    //         caches.match(cacheRequest).then(resp => {
+    //             return resp || fetch(event.request).then(response => {
+    //                 let responseClone = response.clone();
+
+    //                 caches.open(staticCacheName).then(cache => {
+    //                     cache.put(event.request, responseClone)
+    //                 })
+    //                 return response;
+    //             })
+    //         }).catch(err => {
+    //             console.log("err in fetch for " + event.request.url, err);
+    //         })
+    //     )
+    // });    
 })();
